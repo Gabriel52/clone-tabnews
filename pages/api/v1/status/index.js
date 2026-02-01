13d1439001b75f9ba89f21e6fde90e9dabe366d5
Gabriel52 <gabrielbrune52@gmail.com>
2026-02-01 18:22:17 -0300

    feat: update compose file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,12 @@
-function status(_request, response){
+import database from '../../../../infra/database.js';
+
+async function status(_request, response){
+  const result = await database.query('SELECT 1 + 1;')
+  console.log(result)
   response.status(200).json({
-    status: 'ok'
+    status: 'ok',
+    result
   });
 }
 
-export default status;
+export default status; 
