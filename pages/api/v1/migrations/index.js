0c4337b9aaeacf615351a7d850285465cce5da6b
Gabriel52 <gabrielbrune52@gmail.com>
2026-02-18 11:14:12 -0300

    chore: add post migration test

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,32 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(request, response) {
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      migrationsTable: "pgmigrations",
+      dryRun: true,
+      verbose: true,
+    });
+
+    response.status(200).json(migrations);
+  }
+
+  if (request.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      migrationsTable: "pgmigrations",
+      dryRun: true,
+      verbose: true,
+    });
+
+    response.status(200).json(migrations);
+  }
+
+  response.status(405).json({ error: "Method not allowed" });
+}
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,13 @@
+describe("POST to /api/v1/migrations", () => {
+  test("POST to /api/v1/migrations should return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    expect(Array.isArray(responseBody)).toBe(true);
+  });
+});
